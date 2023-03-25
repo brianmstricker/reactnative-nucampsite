f7d950232c70f16f11288018a460ef3a492d374e
Brian Stricker <brianmstricker@gmail.com>
2023-03-25 12:02:23 -0400

    icon, favorite, comments

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,8 +1,7 @@
-import { ScrollView } from "react-native";
+import { ScrollView, Text } from "react-native";
 import { Avatar, Card, ListItem } from "react-native-elements";
 import { PARTNERS } from "../shared/partners";
 import { useState } from "react";
-import { Text } from "react-native";
 
 const Mission = () => {
   return (
