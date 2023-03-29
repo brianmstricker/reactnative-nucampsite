4f5a009dd284fc0f838fae219554b3983a09650a
Brian Stricker <brianmstricker@gmail.com>
2023-03-28 23:15:03 -0400

    redux updates

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,7 +1,7 @@
 import { ScrollView, Text } from "react-native";
 import { Avatar, Card, ListItem } from "react-native-elements";
-import { PARTNERS } from "../shared/partners";
-import { useState } from "react";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
 
 const Mission = () => {
   return (
@@ -22,16 +22,16 @@ const Mission = () => {
 };
 
 const AboutScreen = () => {
-  const [partners, setPartners] = useState(PARTNERS);
+  const partners = useSelector((state) => state.partners);
   return (
     <ScrollView>
       <Mission />
       <Card>
         <Card.Title>Community Partners</Card.Title>
         <Card.Divider />
-        {partners.map((partner) => (
+        {partners.partnersArray.map((partner) => (
           <ListItem key={partner.id}>
-            <Avatar source={partner.image} rounded />
+            <Avatar source={{ uri: baseUrl + partner.image }} rounded />
             <ListItem.Content>
               <ListItem.Title>{partner.name}</ListItem.Title>
               <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
